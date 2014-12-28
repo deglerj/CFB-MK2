5fe34e47e4f6cd5695efc73f32a3a6a23b532aef
deglerj <johannes@degler-online.de>
2014-12-28 20:30:31 +0100

    Scanner now skips empty columns

--- src/org/jd/cfb/Scanner.java
+++ src/org/jd/cfb/Scanner.java
@@ -1,20 +1,32 @@
 package org.jd.cfb;
 
+import java.util.Arrays;
+
 import lejos.nxt.ColorSensor.Color;
 
 public class Scanner {
 
 	public void updateBoard(final Board board, final Operator operator) {
+		final boolean[] blankColumns = new boolean[7];
+		Arrays.fill(blankColumns, false);
+
 		for (int y = 0; y < 6; y++) {
 			final boolean leftToRight = y % 2 == 0;
 
 			for (int i = 0; i < 7; i++) {
 				final int x = leftToRight ? i : 6 - i;
 
+				if (blankColumns[x]) {
+					continue;
+				}
+
 				if (board.getCoin(x, y) == Coin.NONE) {
 					operator.moveTo(x, y);
-					if (!isBlank()) {
-						System.out.println("Player coin @ " + x + "/" + y);
+					if (isBlank()) {
+						blankColumns[x] = true;
+					}
+					else {
+						System.out.println("Your move was: " + x + "/" + y);
 						board.setCoin(x, y, Coin.PLAYER);
 						return;
 					}
